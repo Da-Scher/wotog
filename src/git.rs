ac7550293e883938a805bc515eaed5d109a96aeb
Dak-Scher <dak.scher@proton.me>
2024-05-23 11:40:15 -0700

    added comment headers for functions in git and wotog

--- src/git.rs
+++ src/git.rs
@@ -1,3 +1,7 @@
+/* public function find_git_dir( std::path::PathBuf ) -> Result<std::path::PathBuf, std::io::Error> -- find the .git directory relative to the current directory
+ *  - std::path::PathBuf: dir    -- the current directory
+ *  Returns a Result of Ok(p: std::path::PathBuf) where p is the directory that contains '.git', or Err(std::io::Error) if the directory is not found
+**/
 pub fn find_git_dir(dir: std::path::PathBuf) -> Result<std::path::PathBuf, std::io::Error> {
     // go back a directory until you see that the ".git" directory is a neighbor
     let _ = match std::fs::read_dir(&dir) {
--- src/wotog.rs
+++ src/wotog.rs
@@ -1,6 +1,10 @@
 #[path="git.rs"]
 mod git;
 
+/* public function find_wotog_dir( std::path::PathBuf ) -> Result<std::path::PathBuf, std::io::Error> -- find the .wotog directory relative to the .git directory
+ *  - std::path::PathBuf: dir           -- the directory to start the search from
+ *  Returns a Result of Ok(std::path::PathBuf) or Err(std::io::Error)
+**/
 pub fn find_wotog_dir(dir: std::path::PathBuf) -> Result<std::path::PathBuf, std::io::Error> {
     println!("{}", dir.display());
     // go back a directory until you see that the ".git" directory is a neighbor
@@ -10,17 +14,24 @@ pub fn find_wotog_dir(dir: std::path::PathBuf) -> Result<std::path::PathBuf, std
                 Ok(true)  => return Ok(dir.join(".wotog")),
                 Ok(false) => {
                     match dir.parent() {
+                        // operate recursively until the .git directory is found
                         Some(_) => return find_wotog_dir(dir.parent().unwrap().to_path_buf()),
                         None    => return Err(std::io::Error::new(std::io::ErrorKind::NotFound, "wotog directory not found")),
                     }
                 }
+                // this should never occur because the Ok(false) case should detect running out of directories
                 Err(e) => panic!("{}", e),
             }
         },
-        Err(e) => panic!("{e}"),
+        Err(e) => panic!("{}", e),
     };
 }
 
+/* public function wotog_init( u8, Option<String> ) -> Result<(), std::io::Error> -- create the .wotog directory and config file relative to the .git directory
+ *  - u8: debug_level                   -- the verbosity of the output
+ *  - Option<String>: config_changes    -- the changes to the config file
+ *  Returns a Result of Ok(()) or Err(std::io::Error)
+**/
 pub fn wotog_init(debug_level: u8, config_changes: Option<String>) -> Result<(), std::io::Error> {
     if debug_level == 2 {
         println!("Wotog Init Command");
@@ -38,6 +49,7 @@ pub fn wotog_init(debug_level: u8, config_changes: Option<String>) -> Result<(),
             }
         } 
     };
+    // start from the current directory
     let cwd: std::path::PathBuf = match std::env::current_dir() {
         Ok(d) => d,
         Err(e) => panic!("{e}"),
@@ -72,6 +84,12 @@ pub fn wotog_init(debug_level: u8, config_changes: Option<String>) -> Result<(),
     return Ok(());
 }
 
+/* function wotog_create_config( std::path::PathBuf, Option<String>, u8 ) -> Result<(), std::io::Error> -- create the config file in the .wotog directory
+ *  - std::path::PathBuf: dir           -- the directory to create the config file in
+ *  - Option<String>: changes           -- the changes to the config file
+ *  - u8: debug_level                   -- the verbosity of the output
+ *  Returns a Result of Ok(()) or Err(std::io::Error)
+**/
 fn wotog_create_config(dir: std::path::PathBuf, changes: Option<String>, debug_level: u8) -> Result<(), std::io::Error> {
     if debug_level == 2 {
         println!("wotog_create_config: {:?}, {:?}", dir, changes);
